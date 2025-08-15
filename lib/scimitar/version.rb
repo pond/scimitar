0a11d9c14126001b33da3ed9e34324513ab18e6b
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-08-15 15:38:02 +1200

    v2.12.0, with documentation

--- lib/scimitar/version.rb
+++ lib/scimitar/version.rb
@@ -3,11 +3,11 @@ module Scimitar
   # Gem version. If this changes, be sure to re-run "bundle install" or
   # "bundle update".
   #
-  VERSION = '2.11.0'
+  VERSION = '2.12.0'
 
   # Date for VERSION. If this changes, be sure to re-run "bundle install"
   # or "bundle update".
   #
-  DATE = '2025-03-05'
+  DATE = '2025-08-15'
 
 end
